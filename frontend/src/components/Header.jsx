0d649a5e42c21e121827f1beddebd30f296d21b6
David <dsewagude10@gmail.com>
2023-08-09 23:32:28 +0300

    Product component

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -5,7 +5,7 @@ import logo from "../assets/logo.png";
 const Header = () => {
   return (
     <header>
-      <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
+      <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect>
         <Container>
           <Navbar.Brand href="/">
             <img src={logo} alt="proshop" />
