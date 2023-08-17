5d7e5cfea66104204a3131af5bd91488f6ae7958
David <dsewagude10@gmail.com>
2023-08-17 20:38:35 +0300

    modelling data

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,25 @@
-import express from 'express'
-import dotenv from 'dotenv'
-dotenv.config()
-import products from './data/products.js'
-const port = process.env.PORT || 5000
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+import connectDB from "./config/db.js";
+import products from "./data/products.js";
+const port = process.env.PORT || 5000;
 
-const app = express()
+connectDB();
 
-app.get('/', (req, res)=>{
-    res.send('API is running...')
-})
+const app = express();
 
-app.get('/api/products', (req, res)=>{
-    res.json(products)
-})
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
 
-app.get('/api/products/:id',(req, res)=>{
-    const product = products.find((p) => p._id === req.params.id)
-    res.json(product)
-})
+app.get("/api/products", (req, res) => {
+  res.json(products);
+});
 
-app.listen(port, ()=> console.log(`Server running on port ${port}`))
+app.get("/api/products/:id", (req, res) => {
+  const product = products.find((p) => p._id === req.params.id);
+  res.json(product);
+});
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
