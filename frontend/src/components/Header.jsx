008c57a25c8e3e1133696ed13fc4f78c5db55387
Ssewagudde David <dsewagude10@gmail.com>
2023-10-03 00:52:34 +0300

    Creating Products

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -66,7 +66,7 @@ const Header = () => {
               )}
               {userInfo && userInfo.isAdmin && (
                 <NavDropdown title="Admin" id="adminmenu">
-                  <LinkContainer to="/admin/productslist">
+                  <LinkContainer to="/admin/productlist">
                     <NavDropdown.Item>Products</NavDropdown.Item>
                   </LinkContainer>
                   <LinkContainer to="/admin/userlist">
