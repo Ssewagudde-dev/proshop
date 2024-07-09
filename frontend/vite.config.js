6cbd97b025b5c8400fe4918dcd5f862c71810123
Ssewagudde David <dsewagude10@gmail.com>
2024-07-09 21:11:52 +0300

    production

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,16 +1,29 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import eslintPlugin from "vite-plugin-eslint";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    eslintPlugin({
+      // setup the plugin
+      cache: false,
+      include: ["./src/**/*.js", "./src/**/*.jsx"],
+      exclude: [],
+    }),
+  ],
   server: {
     port: 3000,
+    // proxy: {
+    //   "/api": {
+    //     target: "http://localhost:5000",
+    //     changeOrigin: true,
+    //   },
+    // },
     proxy: {
-      "/api": {
-        target: "http://localhost:5000",
-        changeOrigin: true,
-      },
+      "/api": "http://localhost:5000",
+      "/uploads": "http://localhost:5000",
     },
   },
 });
